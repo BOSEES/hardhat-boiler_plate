d64a973fa0a5589b92711119e9c095a4c27f9849
bosees <rladuddlf3@naver.com>
2023-01-03 13:51:20 +0900

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,34 @@
+import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import { HardhatUserConfig } from "hardhat/types";
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0
+    }
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    localhost: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    }
+  }
+};
+
+export default config;
